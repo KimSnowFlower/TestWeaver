bfa37bf098f1aa0ef253a52bdc02749cf4b97da9
KimSnowFlower <kimbona1148@gmail.com>
2025-10-21 21:12:41 +0900

    feat: #15 add ParameterRepository with findByProjectOrderBySortOrderAsc and findByProjectAndNameIgnoreCase

---
+++ src/main/java/com/testweaver/respository/ParameterRepository.java
@@ -0,0 +1,15 @@
+package com.testweaver.respository;
+
+import com.testweaver.domain.entity.Parameter;
+import com.testweaver.domain.entity.TestProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ParameterRepository extends JpaRepository<Parameter, Long> {
+    List<Parameter> findByProjectOrderBySortOrderAsc(TestProject testProject);
+    
+    Optional<Parameter> findByProjectAndNameIgnoreCase(TestProject testProject, String name);
+    
+}
