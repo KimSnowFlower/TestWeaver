a6146b3a7a543735d9691751143afa9813c26b83
KimSnowFlower <kimbona1148@gmail.com>
2025-10-21 21:14:18 +0900

    feat: #16 add ParameterValueRepository with base method

---
+++ src/main/java/com/testweaver/respository/ParameterValueRepository.java
@@ -0,0 +1,13 @@
+package com.testweaver.respository;
+
+import com.testweaver.domain.entity.Parameter;
+import com.testweaver.domain.entity.ParameterValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ParameterValueRepository extends JpaRepository<ParameterValue, Long> {
+    List<ParameterValue> findByParameterOrderBySortOrderAsc(Parameter parameter);
+    Optional<ParameterValue> findByParameterAndValueIgnoreCase(Parameter parameter, String value);
+}
