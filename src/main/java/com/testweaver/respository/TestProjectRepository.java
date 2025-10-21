b745efd006a50567c3347025fd9be7cf03317d29
KimSnowFlower <kimbona1148@gmail.com>
2025-10-21 21:10:05 +0900

    feat: #14 add TestProjectRepository with findByNameContainingIgnoreCase

---
+++ src/main/java/com/testweaver/respository/TestProjectRepository.java
@@ -0,0 +1,11 @@
+package com.testweaver.respository;
+
+import com.testweaver.domain.entity.TestProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TestProjectRepository extends JpaRepository<TestProject, Long> {
+    List<TestProject> findByNameContainingIgnoreCase(String q);
+    
+}
